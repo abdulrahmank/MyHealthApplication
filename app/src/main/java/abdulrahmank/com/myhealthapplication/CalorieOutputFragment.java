e71ee70ac9665b4972f6b8a154d93cb7728c9fa5
Abdul Rahman <kabdul@thoughtworks.com>
2018-05-18 00:17:22 +0530

    Renames activities

--- app/src/main/java/abdulrahmank/com/myhealthapplication/CalorieOutputFragment.java
+++ app/src/main/java/abdulrahmank/com/myhealthapplication/CalorieOutputFragment.java
@@ -1,6 +1,9 @@
 package abdulrahmank.com.myhealthapplication;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
--- app/src/main/java/abdulrahmank/com/myhealthapplication/CalorieTrackerActivity.java
+++ app/src/main/java/abdulrahmank/com/myhealthapplication/CalorieTrackerActivity.java
@@ -1,30 +1,32 @@
 package abdulrahmank.com.myhealthapplication;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class CalorieTrackerActivity extends AppCompatActivity implements CalorieInputView.CalorieInputFragmentCalorieManager {
+public class CalorieTrackerActivity extends AppCompatActivity implements CalorieInputFragment.CalorieInputFragmentCalorieManager {
 
     private static final int DEFAULT_FULL_CALORIE = 1000;
-    @BindView(R.id.output_container)
-    CalorieOutputView outputView;
-    @BindView(R.id.input_container)
-    CalorieInputView inputView;
+    private CalorieOutputFragment outputFragment;
+    private CalorieInputFragment inputFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_custom_view);
-        ButterKnife.bind(this);
-        inputView.setCalorieManager(this);
+        setContentView(R.layout.activity_main);
+
+        inputFragment = new CalorieInputFragment();
+        inputFragment.setCalorieManager(this);
+        outputFragment = new CalorieOutputFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.input_container, inputFragment).commit();
+        fragmentManager.beginTransaction().replace(R.id.output_container, outputFragment).commit();
     }
 
     @Override
     public void updateCalories(int calories) {
         int calorieProgress = calories * 100 / DEFAULT_FULL_CALORIE;
-        outputView.setCalorieProgress(calorieProgress);
+        outputFragment.setCalorieProgress(calorieProgress);
     }
 }
